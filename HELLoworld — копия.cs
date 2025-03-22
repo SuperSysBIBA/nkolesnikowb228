3feeecf161af96c0f85f6a8f1a4355ce98bea514
SuperSysBIBA <vvkvovavvk@gmail.com>
2025-03-22 16:57:35 +0700

    Add files via upload

---
+++ HELLoworld — копия.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HELLoworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        while wait(0.1)
+        print(HELLoworld)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
